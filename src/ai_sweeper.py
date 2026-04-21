0f6330f6e3f7008005e4c76ba6b2f5f42cda4434
henryhamon <henry@hamonrails.com.br>
2026-04-21 10:14:28 -0300

    feat: update ai_sweeper logic and exclude large dataset files from git

--- src/ai_sweeper.py
+++ src/ai_sweeper.py
@@ -1,4 +1,6 @@
 # solução final 
 def prever_jogada_segura(tabuleiro_atual, modelo):
-    # Seu código aqui
+    # 1. Faça o pré-processamento do tabuleiro_atual (ETL)
+    # 2. Peça ao 'modelo' para fazer a predição (.predict)
+    # 3. Retorne a coordenada X e Y mais segura
     return (x, y)
